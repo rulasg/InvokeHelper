d203291651665db9960a5415982ce9aeb94cd0c0
Raúl (Dibildos) González <rulasg@outlook.com>
2023-12-03 22:35:59 +0000

    consolidate Invoke-MyComand tests

--- InvokeHelperTest/public/Invoke-MyComandWithKey.ps1
+++
@@ -1,9 +0,0 @@
-function InvkeHelperTest_Invoke_MyComandWithKey {
-
-    Set-InvokeCommand -CommandKey GhGetUser -Command 'gh api user'
-
-    $result = Invoke-MyCommandJson -Command 'GhGetUser'
-
-    Assert-AreEqual -Expected 'rulasg' -Presented $result.login
-
-}
--- InvokeHelperTest/public/Invoke-MyCommand.test.ps1
+++ InvokeHelperTest/public/Invoke-MyCommand.test.ps1
@@ -6,3 +6,28 @@ function InvokeHelperTest_MyCommand_Invoke_WhatIF{
     Assert-IsNull -Object $result
     Assert-Contains -Expected $command -Presented $infoVar
 }
+
+function InvkeHelperTest_Invoke_MyComandWithKey {
+
+    Set-InvokeCommand -CommandKey GhGetUser -Command 'gh api user'
+
+    $result = Invoke-MyCommandJson -Command 'GhGetUser'
+
+    Assert-AreEqual -Expected 'rulasg' -Presented $result.login
+
+}
+
+function InvokeHelperTest_MyCommandJson_Invoke{
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '', Scope='Function')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Scope='Function')]
+    param()
+
+    $global:test_json = '{"login": "rulasg","id": 6884408}'
+    $comand = 'echo $global:test_json'
+
+    $result = Invoke-MyCommandJson -Command $comand
+
+    Assert-AreEqual -Expected "rulasg" -Presented $result.login
+    Assert-AreEqual -Expected 6884408 -Presented $result.id
+}
