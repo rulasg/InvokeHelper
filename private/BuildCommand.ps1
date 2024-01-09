57d772c9d6557cbaca5c3e40b8e27e342a4eab20
rulasg <rulasg@outlook.com>
2024-01-09 15:57:24 +0100

    remove previouse  Build-Command

--- private/BuildCommand.ps1
+++ private/BuildCommand.ps1
@@ -1,35 +1,3 @@
-# function Build-Command{
-#     [CmdletBinding()]
-#     [OutputType([string])]
-#     param(
-#         [Parameter(Mandatory,ValueFromPipeline,Position=0)][string]$Command,
-#         [Parameter()][hashtable]$Parameters
-#     )
-#     process {
-#         # Check if command is an alias. If not will return the command.
-#         $cmd1 = Resolve-InvokeCommandAlias -Alias $Command
-
-#         # Replace parameters on command
-#         $cmd1 = Update-CommandWithParameter -Command $cmd1 -Parameters $Parameters
-
-#         # Resolve again checking for full command mocks
-#         $cmd2 = Resolve-InvokeCommandAlias -Alias $cmd1
-
-#         if($cmd1 -ne $cmd2){
-#             # We have a command call mock
-#             # TODO: Allow while chards for mocking commands
-#             "We should check if we are in a sandbox mode" | Write-Verbose
-#         } else {
-#             # TODO: Confirm that we are not on a SandBox Mode
-#             "write a warning if we are not on a sandbox mode" | Write-Verbose
-#         }
-
-#         "Built Script Block: $cmd2" | Write-Verbose
-
-#         return $cmd2
-#     }
-# }
-
 function Build-Command{
     [CmdletBinding()]
     [OutputType([string])]
